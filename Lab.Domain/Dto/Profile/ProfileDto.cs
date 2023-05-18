3ec8c21521df4b7c8022dbac7eb56badbac9890c
BenjaCardozo <90334282+BenjaCardozo@users.noreply.github.com>
2023-05-18 12:10:29 -0300

    ProfileDTO

---
+++ Lab.Domain/Dto/Profile/ProfileDto.cs
@@ -0,0 +1,29 @@
+﻿using Lab.Domain.Dto.Skill;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Dto.Profile
+{
+    public class ProfileDto
+    {
+        public int IdUser { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Mail { get; set; }
+
+        [Required]
+        public IEnumerable<ConsultSkllDto> Skills { get; set; }
+    }
+}
