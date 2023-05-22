97a5ce508f3971059e2a1d7260e331dc2d39ffa3
Emiliano Quattrone <emilianoquattrone008@gmail.com>
2023-05-22 15:38:05 -0300

    Validaciones en ProfileEntity [DataAnnotation]

--- Infraestructure.Entity/Models/ProfileEntity.cs
+++ Infraestructure.Entity/Models/ProfileEntity.cs
@@ -17,19 +17,29 @@ namespace Infraestructure.Entity.Models
 
         [Required]
         [MaxLength(50)]
+        [RegularExpression(@"^[A-Za-z\s]+$", 
+            ErrorMessage = "El nombre solo pueden contener letras y espacios.")]
         public string Name { get; set; }
         [Required]
         [MaxLength(50)]
+        [RegularExpression(@"^[A-Za-z\s]+$",
+            ErrorMessage = "El apellido solo pueden contener letras y espacios.")]
         public string LastName { get; set; }
         [Required]
         [MaxLength(8)]
+        [RegularExpression(@"^[0-9]+$", 
+            ErrorMessage = "El DNI solo pueden contener números.")]
         public int DNI { get; set; }
         [Required]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public string? Description { get; set; }
+        [RegularExpression(@"^[0-9\s-]+$",
+            ErrorMessage = "El Telefono solo pueden contener números.")]
         public string? Phone { get; set; }
         public string? Photo { get; set; }
         [Required]
+        [EmailAddress]
         public string Mail { get; set; }
 
         public string? CV { get; set; }
