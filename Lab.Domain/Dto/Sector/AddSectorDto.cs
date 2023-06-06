5068a842e8e7b22c4560d22828de4acb5144453a
nicolasgodoy <76171813+nicolasgodoy@users.noreply.github.com>
2023-06-06 16:43:46 -0300

    pusheando abm sector

---
+++ Lab.Domain/Dto/Sector/AddSectorDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Dto.Sector
+{
+    public class AddSectorDto
+    {
+        
+        [MaxLength(100)]
+        [RegularExpression(@"^[A-Za-z\s]+$",
+                   ErrorMessage = "La descripcion solo pueden contener letras y espacios.")]
+        public string Description { get; set; }
+    }
+}
---
+++ Lab.Domain/Services/SectorServices.cs
@@ -0,0 +1,30 @@
+﻿using Lab.Domain.Dto.Sector;
+using Lab.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Services
+{
+    public class SectorServices : ISectorServices
+    {
+       
+
+        public List<GetSectorDto> Getall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insert(AddSectorDto add)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
