d600fe77c422e87229e0af01304290fec572603d
Salvador Aguirre <salviagui@gmail.com>
2023-06-14 17:38:11 -0300

    added answer dto's

---
+++ Lab.Domain/Dto/Answer/AddAnswerDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Domain.Dto.Answer
+{
+    public class AddAnswerDto
+    {
+        public string Description { get; set; }
+        public bool IsCorrect { get; set; }
+        public int IdQuestion { get; set; }
+        public int? IdFile { get; set; }
+
+    }
+}
