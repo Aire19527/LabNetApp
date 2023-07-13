5008dec070d658f9e1e9eda32cc681b31f3540a6
nicolasgodoy <76171813+nicolasgodoy@users.noreply.github.com>
2023-07-13 17:02:34 -0300

    addMigration

---
+++ Infraestructure.Core/Migrations/20230713172643_assessmentUserPoints.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Core.Migrations
+{
+    public partial class assessmentUserPoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PointsObtained",
+                table: "AssessmentUser",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "AssessmentUser",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PercentageObtained",
+                table: "AssessmentUser",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "AssessmentUser");
+
+            migrationBuilder.DropColumn(
+                name: "PercentageObtained",
+                table: "AssessmentUser");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PointsObtained",
+                table: "AssessmentUser",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
